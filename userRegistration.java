a936e5741974059e3c7120ca4d1aeae7d0ff3094
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-19 12:42:21 +0530

    Added : Atleast One Special Character

---
+++ userRegistration.java
@@ -0,0 +1,24 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.util.Scanner;
+class Registration{
+   Scanner input =new Scanner(System.in);
+   public void oneSpecialCharacter(){
+      System.out.println("Enter your password must contain min 8 characters :");
+      String passWord=input.nextLine();
+
+      String regex="^(?=.*\\d)(?=.*[A-Z])(?=.*[@#!$%]).{8,20}$";
+      Pattern p=Pattern.compile(regex);
+      Matcher m=p.matcher(passWord);
+      System.out.println("PassWord " +passWord+" is " +m.matches());
+      }
+}
+
+public class userRegistration{
+   public static void main(String[] args){
+      Registration userRegistration=new Registration();
+
+      userRegistration.oneSpecialCharacter();
+   }
+}
